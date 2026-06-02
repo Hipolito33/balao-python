d4fb0eb3eccee162d82a9e29d26566811680ed6c
Arthur Hipólito <hipolitoarthur3622@gmail.com>
2026-06-02 10:05:22 -0300

    feat: implementa jogo do balão

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import *
 
+tela = Screen()
+
 diametro_balao = 40   # diametro inicial do balão
 diametro_balaoPOP = 100   # diametro para estourar o balão
 
@@ -7,6 +9,20 @@ def balao_desenhado(): # desenhando o balão
     color('blue')
     dot(diametro_balao)
 
-balao_desenhado()
+def inflar_balao():
+    global diametro_balao # definindo variável como global
+    diametro_balao = diametro_balao + 10 
+    balao_desenhado() # desenhando o balão com o novo tamanho
 
-tela.mainloop()
+    if diametro_balao >= diametro_balaoPOP:
+        clear()
+        diametro_balao = 40
+        write("POP! 😢")
+    
+
+
+balao_desenhado()
+Screen().onkey(inflar_balao, "Up") # Ativando a função de inflar balão quando a tecla up for ativada
+Screen().listen() #monitora as entradas do teclado
+
+done()
